24972cb1d9ba7e60d806eb5a413a78733de71050
faez <faezc@hotmail.com>
2021-09-23 01:02:15 +0000

    completed pair programming exercise

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//Pair coded with Cynthia Leung @cynthiaaleung on GitHub
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  
+    if (letters.length === 0 || (!word)) {
+      return false;
+    }
+    
+    const horizontalJoin = letters.map(line => line.join(''));
+  
+    if (horizontalJoin.includes(word)) {
+      return true;
+    }
+  
+    let newhorizontalJoin = [];
+  
+    letters.forEach(() => newhorizontalJoin.push([]));
+  
+    for (let row = 0; row < letters.length; row++) {
+      
+      for (let col = 0; col < letters[row].length; col++) {
+        newhorizontalJoin[col].push(letters[row][col]);
+      }
+    }
+  
+    const verticalJoin = newhorizontalJoin.map(line => line.join(''));
+
+    if (verticalJoin.includes(word)) {
+      return true;
+    }
+    return false;
+  };
+  
+  module.exports = wordSearch;
